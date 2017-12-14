309aedf8701c2d7eda60bbc3d76de771eda3db24
Ervin Chua <ervinchua@gmail.com>
2017-12-14 20:22:49 +1100

    Add gulp watcher and some fix some minor issues

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,15 @@ var gulp         = require('gulp')
     stylus       = require('gulp-stylus')
     autoprefixer = require('gulp-autoprefixer')
 
-gulp.task('styles', function() {
+gulp.task('stylus', function() {
   gulp.src('src/stylus/style.styl')
   .pipe(stylus())
   .pipe(autoprefixer({
     browsers: ['last 20 versions']
   }))
   .pipe(gulp.dest('static/css'))
+})
+
+gulp.task('watch', ['stylus'], function() {
+  gulp.watch('src/stylus/**/*', ['stylus'])
 })
