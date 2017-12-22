d9082bf384883e0e13924ec51758e9f7a3c88b6e
Marc Porciuncula <marcporciuncula@gmail.com>
2017-12-22 11:58:17 +1100

    Add sourcemaps for stylus files

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 var gulp         = require('gulp')
     stylus       = require('gulp-stylus')
     autoprefixer = require('gulp-autoprefixer')
+    sourcemaps   = require('gulp-sourcemaps')
 
 gulp.task('stylus', function() {
   gulp.src('src/stylus/style.styl')
+  .pipe(sourcemaps.init())
   .pipe(stylus())
   .pipe(autoprefixer({
     browsers: ['last 20 versions']
   }))
+  .pipe(sourcemaps.write())
   .pipe(gulp.dest('static/css/build'))
 })
 
