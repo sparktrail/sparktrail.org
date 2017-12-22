74d5e9fa970fd9be82b37154d4a9138d93b15807
Marc Porciuncula <marcporciuncula@gmail.com>
2017-12-22 13:09:01 +1100

    Don't crash stylus watch job on error

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp         = require('gulp')
     stylus       = require('gulp-stylus')
     autoprefixer = require('gulp-autoprefixer')
     sourcemaps   = require('gulp-sourcemaps')
+    chalk        = require('chalk')
 
 gulp.task('stylus', function() {
   gulp.src('src/stylus/style.styl')
@@ -14,8 +15,32 @@ gulp.task('stylus', function() {
   .pipe(gulp.dest('static/css/build'))
 })
 
+gulp.task('stylus:dev', function() {
+  // Logs stylus errors to console instead of crashing
+  gulp.src('src/stylus/style.styl')
+  .pipe(stylus())
+  .on('error', function (err) {
+    process.stdout.write(chalk.red.inverse('\n ERROR ') + chalk.red(' Failed to compile.\n\n'))
+
+    const lines = err.message.split('\n');
+    // Make the file easier to read
+    lines.splice(0, 1, chalk.blue(lines[0]) + '\n');
+    // Insert the error descrption below the file
+    lines.splice(1, 0, err.stack.split('\n')[0] + '\n')
+    // Strip the error description at the end
+    lines.splice(lines.length -2, 1);
+
+    process.stdout.write(lines.join('\n') + '\n');
+    this.emit('end')
+  })
+  .pipe(autoprefixer({
+    browsers: ['last 20 versions']
+  }))
+  .pipe(gulp.dest('static/css/build'))
+});
+
 gulp.task('watch', ['stylus'], function() {
-  gulp.watch('src/stylus/**/*', ['stylus'])
+  gulp.watch('src/stylus/**/*', ['stylus:dev'])
 })
 
 gulp.task('build', ['stylus'], function() {})
