43a483f632480ec49f44f7912f3026b352d4baa1
Ervin Chua <ervinchua@gmail.com>
2019-08-02 19:00:08 +1000

    Add mailing list form

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,11 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        sparktrail
-      </h1>
-      <h2 class="subtitle">
-        My impressive Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div class="card">
+      <p class="title">Join our mailing list for updates on internship and graduate opportunities.</p>
+      <form name="newsletter" data-netlify="true">
+        <input type="email" name="email" id="email" placeholder="Email" />
+        <button type="submit" id="submit">Join</button>
+      </form>
     </div>
   </div>
 </template>
@@ -40,6 +22,8 @@ export default {
 
 <style>
 .container {
+  font-family: Inter, 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   margin: 0 auto;
   min-height: 100vh;
   display: flex;
@@ -48,25 +32,46 @@ export default {
   text-align: center;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.card {
+  width: 320px;
+  padding: 48px;
+  background: #fff5e5;
+  border-radius: 4px;
+  box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.15);
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.title {
+  margin-bottom: 48px;
 }
 
 .links {
   padding-top: 15px;
+}
+
+#email {
+  width: 100%;
+  padding: 12px;
+  border: 0px;
+  border-radius: 2px;
+}
+
+#submit {
+  width: 100%;
+  padding: 12px 0;
+  cursor: pointer;
+  margin-top: 16px;
+  font-family: Inter, 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-weight: 500;
+  font-size: 16px;
+  color: #fff;
+  background: #ffa06a;
+  border: 0;
+  border-radius: 2px;
+  letter-spacing: 0.5px;
+}
+
+#submit:hover {
+  background: #ff7d31;
 }
 </style>
